dcc06a0852ea3c366e58b98ddd3ff7ca2f55a016
Marie-Eve Potvin <1864510@csfoy.ca>
2022-02-14 17:33:07 -0500

    fix stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
+  customSyntax: 'postcss-html',
   ignoreFiles: ['**/app.scss', '**/index.scss', 'node_modules/**', '**/libraries/**'],
   rules: {
     'font-weight-notation': null,
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1 class="test">
+    <h1>
       {{ msg }}
     </h1>
     <p>
